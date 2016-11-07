a12f464b46abeab19e944fb55638947d0ffbb159
Dante Del Favero <dante.delfavero@gmail.com>
2016-11-07 23:40:56 +0100

    Aggiornato file deploy per mac

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ dotnet restore
 cd EnerBank.Console
 dotnet build -r osx.10.11-x64
 dotnet publish -r osx.10.10-x64 -c release
-#mv ./bin/release/netcoreapp1.0/publish ./../deploy
+mv ./bin/release/netcoreapp1.0/publish ./../deploy
